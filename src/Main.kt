c03b84a93fde007810c01944b4a91048165de79b
Admin <reteamboss@gmail.com>
2024-09-24 15:58:34 +0300

    2

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,7 @@
 fun main() {
     val list: List<String> = listOf("One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten")
     println(list.groupBy { it.lowercase().first() })
+
+    //Второе задание
+    println(list.groupBy { it.length })
 }
